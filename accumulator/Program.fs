599d291054aa749435ca37250d603dd22f28c177
to884 <toshiki@mokumoku.net>
2022-05-19 15:41:30 +0900

    🔧refactor: Add comments and change var name
    Add comments for details and change variable name

--- accumulator/Program.fs
+++ accumulator/Program.fs
@@ -7,28 +7,35 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
-open Suave.Logging
 open CommandLine
 
 /// コマンドラインオプション
 type Options = {
     [<Option('p', "port", HelpText = "Port number")>] portNumber : int16 option
-    [<Option('h', "host", HelpText = "Host address or name")>] hostName : string option
+    [<Option('h', "host", HelpText = "Host address or name")>] hostAddress : string option
 }
 
 /// タイムスタンプを取得する
 let timestamp =
+    // ロケールに対応するローカルのタイムゾーンを取得する。
     let zone = System.TimeZoneInfo.Local.GetUtcOffset(System.DateTime.Now)
-    let prefix = if (zone<System.TimeSpan.Zero) then "-" else "+"
+    // UTC からの時差を計算し、+ または - のプレフィックスを取得する
+    let prefix = if (zone < System.TimeSpan.Zero) then "-" else "+"
+    // UTC および時差を計算し文字列で返す
     System.DateTime.UtcNow.ToString("yyyyMMddHHmmssffff") + prefix + zone.ToString("hhss")
 
 /// HTTP ヘッダをフォーマットする
 let formatHeaders (headers : (string * string) list) =
     sprintf "Headers : [\n"
-    // (string * string) list を 展開して、(string * string) を連結して string にする
-    + List.fold (fun ac (l, r) -> ac + sprintf "    { %s : %s }\n" l r) "" headers
+    // (string * string) list （キー、値のタプル）のリストを 展開して、
+    // (string * string) を連結し string （"Key : Value"）にする
+    // ac = アキュームレーター
+    // k  = ヘッダのキー
+    // v  = ヘッダの値
+    + List.fold (fun ac (k, v) -> ac + sprintf "    { %s : %s }\n" k v) "" headers
     + "]\n"
 
+/// バイトストリームを UTF-8 文字列に変換する
 let bytesToString (bytes : byte[]) =
    Text.Encoding.UTF8.GetString(bytes)
 
@@ -47,13 +54,22 @@ let format (request : HttpRequest) =
 // let escape = string (char 0x18)
 
 /// ルーティング
+/// Web サーバーのルートテーブル
+/// ここパスと HTTP メソッドとどういう処理をさせるかのマッピングを記述する
 let router =
     choose [
-        // {hostname:port}/accumulater に対する POST を処理する
-        POST >=> path "/accumulate/"
-             >=> request (fun r -> System.Console.WriteLine(format r); OK (format r))
-        PUT  >=> path "/accumulate/"
-             >=> request (fun r -> System.Console.WriteLine(format r); OK (format r))
+        // {hostname:port}/accumulate/ に対する POST を処理する
+        // 標準出力に POST の内容を出力し、レスポンスに同様の内容を返す
+        POST  >=> path "/accumulate/"
+              >=> request (fun r -> System.Console.WriteLine(format r); OK (format r))
+        // {hostname:port}/accumulate/ に対する PUT を処理する
+        // 標準出力に PUT の内容を出力し、レスポンスに同様の内容を返す
+        PUT   >=> path "/accumulate/"
+              >=> request (fun r -> System.Console.WriteLine(format r); OK (format r))
+        // {hostname:port}/accumulate/ に対する PATCH を処理する
+        // 標準出力に PATCH の内容を出力し、レスポンスに同様の内容を返す
+        PATCH >=> path "/accumulate/"
+              >=> request (fun r -> System.Console.WriteLine(format r); OK (format r))
     ]
 
 /// main
@@ -63,24 +79,27 @@ let main argv =
     let result = Parser.Default.ParseArguments<Options>(argv)
     match result with
     | :? Parsed<Options> as parsed -> ()
-    |  _  -> failwith "Invalid Arguments"
+    // パースに失敗した場合は、プログラムを終了する
+    |  _  -> Environment.Exit(-1)
  
     // コマンドラインオプションを設定する 
     let options = (result :?> Parsed<Options>).Value
     let portNumber = match options.portNumber with
                      | Some x -> (int)x
                      | None   -> (int)8080
-    let hostName   = match options.hostName with
-                     | Some x when x = "localhost" -> "127.0.0.1"
-                     | Some x -> x
-                     | None   -> "127.0.0.1"
+    let hostAddress = match options.hostAddress with
+                      | Some x when x = "localhost" -> "127.0.0.1"
+                      | Some x -> x
+                      | None   -> "127.0.0.1"
 
     // Suave のコンフィグレーションを設定する                     
     let cts = new CancellationTokenSource()
     let config = {
         defaultConfig with
+            // 非同期スレッドのキャンセレーショントークン
             cancellationToken = cts.Token
-            bindings = [ HttpBinding.createSimple HTTP hostName portNumber ]
+            // バインディングするアドレスとポート
+            bindings = [ HttpBinding.createSimple HTTP hostAddress portNumber ]
     }
    
     // Suave を起動する 
@@ -88,9 +107,11 @@ let main argv =
     Async.Start(server, cts.Token)
     
     // 任意のキー入力で終了する 
-    printfn "Make requests now"
+    printfn "Press any key to exit"
     Console.ReadKey true |> ignore
-    
+   
+    // キー入力された場合は、Async を中断する 
     cts.Cancel()
-    
+
+    // 正常終了でプログラムを終了する
     0
